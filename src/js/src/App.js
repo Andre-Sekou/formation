bdead3e3f28a144cb863e91ab3414ee7e9cc3304
ZONGO Andre Sekou <aszongo@gmail.com>
2023-11-06 13:37:49 +0000

    TODO ...

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import './App.css';
 import {Avatar, Table} from 'antd';
 import Container from "./Container";
+import {getAllStudents} from "./client";
 
 
 function App(props) {
@@ -27,25 +28,13 @@ function App(props) {
         title: 'Gender', dataIndex: 'gender', key: 'gender'
     }]
 
-    // setStudent((student) => {
-    //         return [
-    //             student = getAllStudents()
-    //         ]
-    //     }
-    // );
-    const d = [
-        {
-            studentId: '2', firstName: 'ZONGO', lastName: 'Sekou', email: 'aszongo@gmail.com', gender: 'MALE'
-        },
-        {
-            studentId: '2', firstName: 'ZONGO', lastName: 'Sekou', email: 'aszongo@gmail.com', gender: 'MALE'
-        },
-        {
-            studentId: '2', firstName: 'ZONGO', lastName: 'Sekou', email: 'aszongo@gmail.com', gender: 'MALE'
-        }
-    ]
+    getAllStudents().then((res) =>
+        setStudent((prev) => {
+            return ({...prev, student: res.json()});
+        }));
+
     return (<Container>
-        <Table dataSource={[d]} columns={columns} pagination={false} rowKey='studentId'/>
+        <Table dataSource={student.data} columns={columns} pagination={false} rowKey='studentId'/>
     </Container>)
 
 }
