70ca752a0067f5290e6c1d66c164e4861e7fa8df
ZONGO Andre Sekou <aszongo@gmail.com>
2023-11-09 18:19:39 +0000

    Initializing Posgtres DB from Docker and application.yml

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 
 import './App.css';
 import {Avatar, Table} from 'antd';
@@ -8,69 +8,36 @@ import {getAllStudents} from "./client";
 
 function App(props) {
 
-    const [student, setStudent] = useState([]);
+   const [student, setStudent] = useState([]);
 
-    useEffect(() => {
-        getStudents();
-    }, []);
-
-    const getStudents = () => {
-        getAllStudents().then((res) => {
-            setStudent(res);
-        });
-    }
-
-    return (
-        student && student.length > 0 && (
-            <Container>
-                {/*TODO*/}
-                <Table
-                    dataSource={student}
-                    columns={columns}
-                    pagination={false}
-                    rowKey='studentId'
-                />
-            </Container>
-        ))
-}
-
-const columns = [
-    {
-        title: '',
-        key: 'avatar',
-        render: (text, student) => {
-            return (
-                <Avatar size='large'>
-                    {`${student.firstName.charAt(0).toUpperCase()}`}
-                </Avatar>
-            )
+    const columns = [
+        {title: '', key: 'avatar', render: (text, student) => {
+            <Avatar size='large'>
+                {`${student.firstName.charAt(0).toUpperCase()}`}
+            </Avatar>
         }
-    },
-    {
-        title: 'Student Id',
-        dataIndex: 'studentId',
-        key: 'studentId'
-    },
-    {
-        title: 'First Name',
-        dataIndex: 'firstName',
-        key: 'firstName'
-    },
-    {
-        title: 'Last Name',
-        dataIndex: 'lastName',
-        key: 'lastName'
-    },
-    {
-        title: 'Email',
-        dataIndex: 'email',
-        key: 'email'
-    },
-    {
-        title: 'Gender',
-        dataIndex: 'gender',
-        key: 'gender'
-    }
-];
+    }, {
+        title: 'Student Id', dataIndex: 'studentId', key: 'studentId'
+    }, {
+        title: 'First Name', dataIndex: 'firstName', key: 'firstName'
+    }, {
+        title: 'Last Name', dataIndex: 'lastName', key: 'lastName'
+    }, {
+        title: 'Email', dataIndex: 'email', key: 'email'
+    }, {
+        title: 'Gender', dataIndex: 'gender', key: 'gender'
+    }]
+
+    getAllStudents().then((res) =>
+        setStudent((prev) => {
+            return ({...prev, student: res.json()});
+        }));
+
+    return (<Container>
+        {/*TODO*/}
+        <Table dataSource={student.data} columns={columns} pagination={false} rowKey='studentId'/>
+    </Container>)
+
+}
 
 export default App;
--- src/main/java/com/timewise/formation/FormationApplication.java
+++ src/main/java/com/timewise/formation/FormationApplication.java
@@ -2,8 +2,11 @@ package com.timewise.formation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+import javax.sql.DataSource;
+
+@SpringBootApplication()
 public class FormationApplication {
 
 	public static void main(String[] args) {
