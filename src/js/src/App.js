77bf6f3a0553f0aba7f8d7c13b492a331f67d5dc
Edouard Zerbo <zerboedouard@gmail.com>
2023-11-09 10:08:26 -0500

    Refactor Code to Use Hooks

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import './App.css';
 import {Avatar, Table} from 'antd';
@@ -8,36 +8,69 @@ import {getAllStudents} from "./client";
 
 function App(props) {
 
-   const [student, setStudent] = useState([]);
+    const [student, setStudent] = useState([]);
 
-    const columns = [
-        {title: '', key: 'avatar', render: (text, student) => {
-            <Avatar size='large'>
-                {`${student.firstName.charAt(0).toUpperCase()}`}
-            </Avatar>
-        }
-    }, {
-        title: 'Student Id', dataIndex: 'studentId', key: 'studentId'
-    }, {
-        title: 'First Name', dataIndex: 'firstName', key: 'firstName'
-    }, {
-        title: 'Last Name', dataIndex: 'lastName', key: 'lastName'
-    }, {
-        title: 'Email', dataIndex: 'email', key: 'email'
-    }, {
-        title: 'Gender', dataIndex: 'gender', key: 'gender'
-    }]
+    useEffect(() => {
+        getStudents();
+    }, []);
 
-    getAllStudents().then((res) =>
-        setStudent((prev) => {
-            return ({...prev, student: res.json()});
-        }));
+    const getStudents = () => {
+        getAllStudents().then((res) => {
+            setStudent(res);
+        });
+    }
 
-    return (<Container>
-        {/*TODO*/}
-        <Table dataSource={student.data} columns={columns} pagination={false} rowKey='studentId'/>
-    </Container>)
-
+    return (
+        student && student.length > 0 && (
+            <Container>
+                {/*TODO*/}
+                <Table
+                    dataSource={student}
+                    columns={columns}
+                    pagination={false}
+                    rowKey='studentId'
+                />
+            </Container>
+        ))
 }
+
+const columns = [
+    {
+        title: '',
+        key: 'avatar',
+        render: (text, student) => {
+            return (
+                <Avatar size='large'>
+                    {`${student.firstName.charAt(0).toUpperCase()}`}
+                </Avatar>
+            )
+        }
+    },
+    {
+        title: 'Student Id',
+        dataIndex: 'studentId',
+        key: 'studentId'
+    },
+    {
+        title: 'First Name',
+        dataIndex: 'firstName',
+        key: 'firstName'
+    },
+    {
+        title: 'Last Name',
+        dataIndex: 'lastName',
+        key: 'lastName'
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email'
+    },
+    {
+        title: 'Gender',
+        dataIndex: 'gender',
+        key: 'gender'
+    }
+];
 
 export default App;
